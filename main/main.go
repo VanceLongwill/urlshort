43618fd325aeaf72947d59360e5522b279666117
Vance Longwill <vancelongwill@gmail.com>
2019-01-14 21:41:12 +0100

    add optional yaml file as input

--- main/main.go
+++ main/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
-	"github.com/gophercises/urlshort"
+	".."
 )
 
 func main() {
+
+	// yamlFilename := flag.String("f", "mock.yaml", "yaml file")
+	// flag.Parse()
+
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -16,21 +22,21 @@ func main() {
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	fmt.Println(mapHandler)
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	// file, err = os.Open(*yamlFilename)
+	// if fileErr != nil {
+	// 	fmt.Print(fileErr, "\nFailed to read the YAML file provided")
+	// 	os.Exit(1)
+	// }
+	// yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Starting the server on :8080")
+	// http.ListenAndServe(":8080", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
